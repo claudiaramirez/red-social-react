e6ba5db621db5d0c22ee32a9a9e04d13a67f6dea
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-06-25 13:36:11 -0500

    inicio de sesion

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// const Firebase = () => (
+//     <div>
+//     <h1>Landing</h1>
+//     </div>
+// );
+// export default Firebase;
+
+import FirebaseContext, { withFirebase } from './context';
+import Firebase from './firebase';
+export default Firebase;
+export { FirebaseContext ,withFirebase};
+
